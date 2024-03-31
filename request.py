11344523808534150876534dcc4eb2ebf599bf1f
OlejaCat <118455092+OlejaCat@users.noreply.github.com>
2024-03-31 13:41:22 +0300

    Update request.py

--- request.py
+++ request.py
@@ -4,22 +4,22 @@ import numpy as np
 from datetime import datetime
 
 
-def get_rooms_numbers(windows_for_room: dict, windows: list[int], date: int):
-    windows_for_room_list = sorted(list(windows_for_room.items()), key=lambda x: x[0])
-    windows_for_room_list = [i for _, i in windows_for_room_list]
-    height = len(windows_for_room_list)
-    l, width = len(windows), sum(windows)
+def get_rooms_numbers(windows: dict, windows_for_room: list[int], date: int):
+    windows_list = sorted(list(windows.items()), key=lambda x: x[0])
+    windows_list = [i for _, i in windows_list]
+    height = len(windows_list)
+    l, width = len(windows_for_room), sum(windows_for_room)
     arr = np.arange(1, l * height + 1).reshape(height, l)[::-1].tolist()
 
     for idx, level in enumerate(arr):
         new_line = []
-        for j, i in enumerate(windows):
+        for j, i in enumerate(windows_for_room):
             new_line += [level[j]] * i
         arr[idx] = new_line
 
     light_coordinates = []
     rooms = set()
-    for idx, level in enumerate(windows_for_room_list[::-1]):
+    for idx, level in enumerate(windows_list[::-1]):
         for j, condition in enumerate(level):
             if condition:
                 light_coordinates.append((idx, j))
@@ -28,7 +28,18 @@ def get_rooms_numbers(windows_for_room: dict, windows: list[int], date: int):
     rooms = list(rooms)
     rooms.sort()
 
-    return arr, light_coordinates, rooms, len(rooms), date
+    result = {
+        "home": arr,
+        "light": light_coordinates,
+        "rooms": rooms,
+        "count": len(rooms),
+        "date": date,
+        "correct": test_rooms_correct(rooms, len(rooms), date),
+        "height": len(arr),
+        "width": len(arr[0])
+    }
+
+    return result
 
 
 def test_rooms_correct(rooms: list[int], number: int, date: int):
@@ -50,19 +61,27 @@ def test_rooms_correct(rooms: list[int], number: int, date: int):
         "X-Auth-Token": "ppo_10_17608"
     }
 
-    resp = requests.post(url, data=json.dumps(d), headers=headers)
-    return resp.json()
+    response = requests.post(url, data=json.dumps(d), headers=headers)
+    return response.status_code == 200
 
 
-response = requests.get(
-    "https://olimp.miet.ru/ppo_it_final",
-    headers={"X-Auth-Token": "ppo_11_30013"},
-    params={
-        "day": "25",
-        "month": "01",
-        "year": "23"
-    }
-)
+def get_data_from_date(date: str):
+    day, month, year = date.split("-")
+    response = requests.get(
+        "https://olimp.miet.ru/ppo_it_final",
+        headers={"X-Auth-Token": "ppo_11_30013"},
+        params={
+            "day": day,
+            "month": month,
+            "year": year
+        }
+    )
+
+    body = response.json()["message"]
+    windows_for_flat = body["windows_for_flat"]["data"]
+    windows = body["windows"]["data"]
+    date = body["date"]["data"]
+    return get_rooms_numbers(windows, windows_for_flat, date)
 
 
-
+print(get_data_from_date("25-01-23"))
