b8dbd4689d0f8f9f5ad11d44eeb3eeaa3e01ed46
P1Jack <118741123+P1Jack@users.noreply.github.com>
2024-03-31 13:19:57 +0300

    Update sqll.py

--- sqll.py
+++ sqll.py
@@ -11,7 +11,8 @@ def init_database():
                    windows TEXT,
                    lights TEXT,
                    count INTEGER,
-                   wall, coords);""")
+                   wall TEXT,
+                   coords TEXT);""")
     con.commit()
     return True
 
@@ -19,9 +20,9 @@ def init_database():
 def save_day(data):
     con = sqlite3.connect('predprof.db')
     cur = con.cursor()
-    cur.execute("""INSERT INTO dayz (date, floor_num, windows, lights, count) 
-                           VALUES (?, ?, ?, ?, ?)
-                        """, (data[0], data[1], data[2], data[3], data[4],))
+    cur.execute("""INSERT INTO dayz (date, floors, windows, lights, count, wall, coords) 
+                           VALUES (?, ?, ?, ?, ?, ?, ?)
+                        """, (data[0], data[1], data[2], data[3], data[4], data[5], data[6]))
     con.commit()
     return True
 
@@ -29,19 +30,12 @@ def save_day(data):
 def get_day(date):
     con = sqlite3.connect('predprof.db')
     cur = con.cursor()
-    try:
-        data = cur.execute("""SELECT floor_num, windows, lights, count FROM dayz WHERE date=?""", (date,)).fetchone()[0]
-    except Exception as e:
-        return str(e)[26:]
-    print(date)
-    # return_context = {for el in data}
-    # return return_context
-
-
-def get_all():
-    con = sqlite3.connect('predprof.db')
-    cur = con.cursor()
-    data_all = cur.execute("""SELECT * FROM dayz""").fetchall()
-    return data_all
-
-
+    if not date:
+        data_all = cur.execute("""SELECT * FROM dayz""").fetchall()
+    else:
+        try:
+            data_all = cur.execute("""SELECT floor_num, windows, lights, count FROM dayz WHERE date=?""", (date,)).fetchall()
+        except Exception as e:
+            return str(e)[26:]
+    context = {el[0]: [el[1], el[2], el[3], el[4], el[5], el[6]] for el in data_all}
+    return context
